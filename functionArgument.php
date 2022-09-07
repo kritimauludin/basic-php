410cb0ff04f78a532d0b998bb7ebd67c75ff7829
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-09-07 11:47:22 +0700

    Update functionArgument.php

--- functionArgument.php
+++ functionArgument.php
@@ -1,19 +1,35 @@
 <?php
-function hello($name = "tidak dikenal")
+//function parameter with default argument value
+//biasanya kesalahan yg terjadi jika ada 2 parameter dan default value hanya ada diparameter awal, sehingga akan terjadi eror
+//lebih baik jika default value ada diakhir
+function hello($firstName, $lastName = "")
 {
-    echo "$name" . PHP_EOL;
+    echo "Hello $firstName $lastName" . PHP_EOL;
 }
 hello();
 hello("kriti");
+hello("kriti", "mauludin");
 
+//type declaration function
 function sum(int $a, int $b)
 {
     $total = $a + $b;
-    echo $total . PHP_EOL;
+    echo "$a + $b = $total" . PHP_EOL;
 }
-
+sum("100", "100");
 sum(100, 100);
+sum(true, false);
+// error sum([], []);
 
+
+/*
+
+*Variable-length argument list merupakan kemampuan dimana kita bisa membuat sebuah parameter yang menerima banyak value
+* Variable-length argument list secara otomatis akan membuat argument tersebut menjadi array, namun kita tidak perlu manual mengirim array ke functionnya
+* Variable-length argument list hanya bisa dilakukan di argument posisi terakhir
+* Untuk membuat variable-length argument list, kita bisa menggunakan tanda … (titik tiga kali) sebelum nama argument
+
+*/
 function sumAll(...$values)
 {
     $total = 0;
@@ -22,6 +38,6 @@ function sumAll(...$values)
     }
     echo "total " . implode(",", $values) . " = $total" . PHP_EOL;
 }
-
+$values = [10, 5, 6, 8];
 sumAll(10, 5, 6, 8);
-sumAll(...[10, 5, 6, 8]);
+sumAll(...$values);
