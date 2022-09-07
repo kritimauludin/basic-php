0069851ef0efb185ce7109b9faa1ecfe750916fa
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-09-07 11:26:51 +0700

    Update nullCloascingOperator.php

--- nullCloascingOperator.php
+++ nullCloascingOperator.php
@@ -1,6 +1,15 @@
 <?php
+/*
+
+* Sebelumnya kita tahu bahwa data kosong direpresentasikan dengan data NULL di PHP
+* Dan hal yang paling repot di PHP adalah mengecek apakah sebuah data ada atau tidak, dan juga apakah data tersebut isinya null atau bukan
+* Biasanya untuk melakukan itu, kita perlu menggunakan pengecekan if statement dengan function isset($variable)
+* Namun untungnya di php ada null coalescing operator menggunakan tanda ??
+
+*/
 $data = ["action" => "Create"];
 
+//tanpa null coalescing operator
 if (isset($data["action"])) {
     $action = $data["action"];
 } else {
@@ -8,6 +17,7 @@ if (isset($data["action"])) {
 }
 echo $action . PHP_EOL;
 
+//dengan null coalescing operator
 $data = [];
 $action = $data['action'] ?? 'nothing';
 echo $action . PHP_EOL;
