65c101f0f53c50256605951f058095b02ae23e06
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-09-07 11:21:37 +0700

    Update goto.php

--- goto.php
+++ goto.php
@@ -10,3 +10,15 @@
 
  a:
  echo "huruf a";
+
+$counter = 1;
+while(true){
+  echo "While loop $counter". PHP_EOL;
+  $counter++
+   if($counter > 10) {
+      goto end;
+   }
+}
+
+end:
+echo "end loop";
