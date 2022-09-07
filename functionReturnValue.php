c59974488476f888ee8bf489b9241d33100329fe
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-09-07 11:55:08 +0700

    Update functionReturnValue.php

--- functionReturnValue.php
+++ functionReturnValue.php
@@ -1,5 +1,12 @@
 <?php
-function sum(int $a, int $b)
+/*
+* Secara default, function itu tidak menghasilkan value apapun, namun jika kita ingin, kita bisa membuat sebuah function mengembalikan nilai
+* Dan di dalam block function, untuk menghasilkan nilai tersebut, kita harus menggunakan kata kunci return, lalu diikuti dengan data yang ingin dihasilkan
+* Kita hanya bisa menghasilkan 1 data di sebuah function, tidak bisa lebih dari satu
+*/
+
+//return type declaration
+function sum(int $a, int $b): int
 {
     return $a + $b;
 }
