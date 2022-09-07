404569392f71810ccd10b5f04098663da97354c4
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-09-07 11:17:44 +0700

    Update goto.php

--- goto.php
+++ goto.php
@@ -1,3 +1,9 @@
+/*
+* Salah satu fitur yang sebenarnya jarang sekali digunakan di PHP adalah goto operator
+* Kenapa jarang digunakan karena jika terlalu banyak menggunakan goto operator, kode program aplikasinya akan mudah membingungkan yang membaca kode nya
+* goto adalah fitur dimana kita bisa loncat ke kode program sesuai dengan keinginan kita
+* Agar goto bisa loncat ke kode program, kita harus membuat label di php dengan menggunakan nama label lalu diakhiri : (colon)
+*/
 <?php 
  goto a;
  echo "hello";
